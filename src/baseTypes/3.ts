83fc53c342743be6aacfc1bdabe5413a11b2f183
vovaukrainets <127029812+gitvova@users.noreply.github.com>
2023-10-14 21:19:54 +0300

    corrected
    corrected

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -3,4 +3,10 @@ some = "Text";
 let str: string;
 str = some as string;
 
+if (typeof some === "string") {
+  str = some as string;
+} else {
+  console.log("Змінна 'some' не є рядком.");
+}
+
 export {};
